1e0847e223145cdd993993665f2a5e3524c07154
bjscoob <31317956+bjscoob@users.noreply.github.com>
2022-06-23 23:28:28 -0400

    v2

--- src/App.js
+++ src/App.js
@@ -2,26 +2,39 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div class="imgbox">
-    <img alt="bg" class="center-fit" src='https://jax-apps.com/images/JA_BG.svg'/>
-    <div class="sky">
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
-      <div class="star"></div>
+    <div id="pageCont">
+      <div class="imgbox">
+        <img
+          alt="bg"
+          class="center-fit"
+          src="https://jax-apps.com/images/JA_BG.svg"
+        />
+        <div class="sky">
+          <div class="star"></div>
+          <div class="star"></div>
+          <div class="star"></div>
+          <div class="star"></div>
+          <div class="star"></div>
+          <div class="star"></div>
+          <div class="star"></div>
+        </div>
+        <div class="sky2">
+          <div class="star2"></div>
+          <div class="star2"></div>
+          <div class="star2"></div>
+          <div class="star2"></div>
+          <div class="star2"></div>
+          <div class="star2"></div>
+          <div class="star2"></div>
+        </div>
       </div>
-      <div class="sky2">
-      <div class="star2"></div>
-      <div class="star2"></div>
-      <div class="star2"></div>
-      <div class="star2"></div>
-      <div class="star2"></div>
-      <div class="star2"></div>
-      <div class="star2"></div>
+      <div class="logobox">
+        <img
+          alt="logo"
+          id="logo"
+          src="https://jax-apps.com/images/ja_logo.svg"
+        />
       </div>
-      </div>
+    </div>
   );
 }
