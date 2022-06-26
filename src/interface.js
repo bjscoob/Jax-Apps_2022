c86467447519f3e37b32af7f42c7c33033c7778d
bjscoob <31317956+bjscoob@users.noreply.github.com>
2022-06-26 11:11:53 -0400

    v6

--- src/interface.js
+++ src/interface.js
@@ -5,10 +5,36 @@ class Interface extends React.Component {
     return (
       <div class="interfaceGrid">
         <div class="menuGrid">
-          <button class="menuButton">Home</button>
-          <button class="menuButton">Resume</button>
-          <button class="menuButton">About Me</button>
-          <button class="menuButton">Contact</button>
+          <div class="flip-container">
+            <div class="flipper">
+              <div class="front1">
+                <button class="menuButton" id="btn1" onclick="">
+                  HOME
+                </button>
+              </div>
+              <div class="back"></div>
+            </div>
+          </div>
+          <div class="flip-container">
+            <div class="flipper">
+              <div class="front2">
+                <button class="menuButton" id="btn2">
+                  ABOUT ME
+                </button>
+              </div>
+              <div class="back"></div>
+            </div>
+          </div>
+          <div class="flip-container">
+            <div class="flipper">
+              <div class="front3">
+                <button class="menuButton" id="btn3">
+                  CONTACT
+                </button>
+              </div>
+              <div class="back"></div>
+            </div>
+          </div>
         </div>
         <div class="grid-item">2</div>
         <div class="grid-item">3</div>
